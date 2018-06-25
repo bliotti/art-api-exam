aeb01372ae915f37d0db2dc1d585954b6769b7cf
Brian Liotti <bdliotti@gmail.com>
2018-06-25 11:33:27 -0400

    POST

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 5001
 const bodyParser = require('body-parser')
 const NodeHTTPError = require('node-http-error')
-const {} = require('./dal')
+const { addPainting } = require('./dal')
 const {
 	propOr,
 	map,
@@ -15,11 +15,46 @@ const {
 	pathOr,
 	filter
 } = require('ramda')
+const reqFieldChecker = require('./lib/required-field-check')
+const requiredFields = ['name', 'movement', 'artist', 'yearCreated', 'museum']
 
 app.use(bodyParser.json())
 
 app.get('/', function(req, res, next) {
 	res.send('Welcome to the Art API. Manage all the paintings.')
+})
+
+app.post('/paintings', function(req, res, next) {
+	const newArt = propOr({}, 'body', req)
+
+	if (isEmpty(newArt)) {
+		next(
+			new NodeHTTPError(
+				400,
+				'Nothing was found in the request body. Confirm Content-Type is application/json.'
+			)
+		)
+	}
+
+	const missingFields = reqFieldChecker(requiredFields, newArt)
+
+	const sendMissingFieldError = compose(
+		not,
+		isEmpty
+	)(missingFields)
+
+	if (sendMissingFieldError) {
+		next(
+			new NodeHTTPError(
+				400,
+				`Missing the following required fields: ${join(', ', missingFields)}`
+			)
+		)
+	}
+
+	addPainting(newArt)
+		.then(result => res.status(201).send(result))
+		.catch(err => next(new NodeHTTPError(err.status, err.message, err)))
 })
 
 app.use((err, req, res, next) => {
--- dal.js
+++ dal.js
@@ -1,1 +1,22 @@
+require('dotenv').config()
+const PouchDB = require('pouchdb-core')
+PouchDB.plugin(require('pouchdb-adapter-http'))
 
+const { merge, prop, map } = require('ramda')
+const pkGen = require('./lib/pk-gen')
+
+const db = new PouchDB(
+	`${process.env.COUCH_HOSTNAME}${process.env.COUCH_DBNAME}`
+)
+
+const addPainting = painting => {
+	const modifiedPainting = merge(painting, {
+		type: 'painting',
+		_id: pkGen('painting_', '_', prop('name', painting))
+	})
+	return db.put(modifiedPainting)
+}
+
+module.exports = {
+	addPainting
+}
